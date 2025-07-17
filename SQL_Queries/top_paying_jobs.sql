dba0ac3d9823955763170e89af4a3a830132743b
Pedro Daim <daimhpedro@gmail.com>
2025-07-17 18:14:47 -0300

    --Updated the main CTE to include top 10 data scientist roles and top 10 data engineer roles.

--- SQL_Queries/top_paying_jobs.sql
+++ SQL_Queries/top_paying_jobs.sql
@@ -1,7 +1,7 @@
-/*
- * SQL Query to find the top paying Data Analyst jobs in the United States
- * This CTE retrieves job postings for Data Analysts, filtering by salary and location.
- */
+/* This SQL query retrieves the top paying data jobs in the United States,
+   specifically focusing on Data Analyst, Data Scientist, and Data Engineer roles.
+   It combines results from three separate queries for each job category 
+*/
 
 WITH top_data_analyst_jobs AS (
     SELECT
@@ -22,6 +22,53 @@ WITH top_data_analyst_jobs AS (
     ORDER BY
         salary_year_avg DESC
     LIMIT 10
+),
+top_data_scientist_jobs AS (
+    SELECT
+        job_id,
+        job_title_short,
+        job_title,
+        job_location,
+        job_country,
+        salary_year_avg,
+        company_id,
+        'Data Scientist' AS job_category
+    FROM
+        job_postings_fact
+    WHERE
+        salary_year_avg IS NOT NULL
+        AND job_country = 'United States'
+        AND job_title_short ILIKE '%Data Scientist%'
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+),
+top_data_engineer_jobs AS (
+    SELECT
+        job_id,
+        job_title_short,
+        job_title,
+        job_location,
+        job_country,
+        salary_year_avg,
+        company_id,
+        'Data Engineer' AS job_category
+    FROM
+        job_postings_fact
+    WHERE
+        salary_year_avg IS NOT NULL
+        AND job_country = 'United States'
+        AND job_title_short ILIKE '%Data Engineer%'
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
 )
-
+-- Combine all the top jobs
 SELECT * FROM top_data_analyst_jobs
+UNION ALL
+SELECT * FROM top_data_scientist_jobs
+UNION ALL
+SELECT * FROM top_data_engineer_jobs
+ORDER BY
+    job_category, salary_year_avg DESC;
+
