357b5932b993948ad869a4220f3e5a0a00e42b87
Pedro Daim <daimhpedro@gmail.com>
2025-07-22 18:18:50 -0300

    - Feature added query for business question 2 which build on the first query answering business question 1.

--- SQL_Queries/top_paying_jobs.sql
+++ SQL_Queries/top_paying_jobs.sql
@@ -62,13 +62,66 @@ top_data_engineer_jobs AS (
     ORDER BY
         salary_year_avg DESC
     LIMIT 10
-)
--- Combine all the top jobs
-SELECT * FROM top_data_analyst_jobs
-UNION ALL
-SELECT * FROM top_data_scientist_jobs
-UNION ALL
-SELECT * FROM top_data_engineer_jobs
-ORDER BY
-    job_category, salary_year_avg DESC;
+),
 
+-- Combine all the top jobs into a single reusable CTE
+all_top_jobs AS (
+    SELECT * FROM top_data_analyst_jobs
+    UNION ALL
+    SELECT * FROM top_data_scientist_jobs
+    UNION ALL
+    SELECT * FROM top_data_engineer_jobs
+),
+
+-- New CTE to get skill counts per category and total skills per category
+skill_counts_by_category AS (
+    SELECT
+        job_category,
+        s.skills AS skill_name,
+        COUNT(s.skills) AS skill_count
+    FROM
+        all_top_jobs AS atj
+    JOIN
+        skills_job_dim AS sjd ON atj.job_id = sjd.job_id
+    JOIN
+        skills_dim AS s ON sjd.skill_id = s.skill_id
+    GROUP BY
+        atj.job_category,
+        s.skills
+)
+-- #######################################################
+-- ## END OF BASE CTEs ##
+-- #######################################################
+
+--QUESTION 1: Top-Paying Jobs Overview##
+-- #######################################################
+/*
+Business Question:
+What are the top-paying jobs for Data Analysts, Data Scientists, and Data Engineers across the USA?
+*/
+-- This query retrieves the top-paying jobs for each category and combines them into a single result set
+
+--SELECT * FROM all_top_jobs
+--ORDER BY job_category, salary_year_avg DESC;
+
+/*
+-- #######################################################
+-- ## QUESTION 2: Most Frequently Required Skills for Top-Paying Jobs ##
+-- #######################################################
+
+Business Question:
+What are the most frequently required skills for the top-paying Data Analyst, Data Scientist, and Data Engineer jobs in the USA?
+*/
+SELECT
+    s.skills AS skill_name,
+    COUNT(*) AS skill_count
+FROM
+    all_top_jobs AS atj
+JOIN
+    skills_job_dim AS sjd ON atj.job_id = sjd.job_id
+JOIN
+    skills_dim AS s ON sjd.skill_id = s.skill_id
+GROUP BY
+    s.skills_name
+ORDER BY
+    skill_count DESC;
